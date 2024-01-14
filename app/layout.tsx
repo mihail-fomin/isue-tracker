50acf993ba1060536459169a247fbe9eea3c4a6c
misha133kv <mihail_fomin_95@mail.ru>
2024-01-14 19:23:04 +0500

    Set up Radix ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import NavBar from './NavBar'
-
+import '@radix-ui/themes/styles.css';
 const inter = Inter({ subsets: ['latin'] })
+import { Theme } from '@radix-ui/themes';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -13,8 +14,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <NavBar />
-      <main>{children}</main>
+      <body className={inter.className}>
+      <Theme>
+        <NavBar />
+        <main>{children}</main>
+      </Theme>
+      </body>
     </html>
   )
 }
