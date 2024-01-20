b989070b6be1decbce9441ccce0a9379029535e2
misha133kv <mihail_fomin_95@mail.ru>
2024-01-20 12:19:39 +0500

    Added additional loading skeletons

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { Box, Card, Flex } from '@radix-ui/themes'
+import delay from 'delay'
+import Skeleton from 'react-loading-skeleton'
 
-const LoadingIssuesPage = () => {
-  return <div>Loading...</div>
+const LoadingIssuesPage = async () => {
+  await delay(2000)
+  return (
+    <Box className='max-w-xl'>
+      <Skeleton />
+      <Flex className='space-x-3 my-2'>
+        <Skeleton width='5rem'/>
+        <Skeleton width='8rem'/>
+      </Flex>
+      <Card className='prose mt-4'>
+        <Skeleton count={3}/>
+      </Card>
+    </Box>
+  )
+  
 }
 
 export default LoadingIssuesPage
