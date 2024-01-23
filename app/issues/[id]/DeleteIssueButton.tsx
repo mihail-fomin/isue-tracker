29743511d2a5132b3ad77305042b87a57a1a7e4f
misha133kv <mihail_fomin_95@mail.ru>
2024-01-23 19:58:12 +0500

    Added delete Button

---
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@radix-ui/themes'
+import React from 'react'
+
+const DeleteIssueButton = ({ issueId }: { issueId: string}) => {
+  return (
+    <Button>Delete Issue</Button>
+  )
+}
+
+export default DeleteIssueButton
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
+import React from 'react'
 import type { Metadata } from 'next'
 import './theme-config.css'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import NavBar from './NavBar'
 import '@radix-ui/themes/styles.css'
-import { Theme, ThemePanel } from '@radix-ui/themes'
+import { Container, Theme } from '@radix-ui/themes'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -22,7 +23,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={inter.variable}>
         <Theme appearance="light" accentColor="purple">
           <NavBar />
-          <main className="p-5">{children}</main>
+          <main className="p-5">
+            <Container>
+              {children}
+            </Container>
+          </main>
           {/* <ThemePanel /> */}
         </Theme>
       </body>
