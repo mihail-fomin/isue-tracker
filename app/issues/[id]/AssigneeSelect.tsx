60f590a47cab991d2e531f47c9c47ce141cc397f
misha133kv <mihail_fomin_95@mail.ru>
2024-02-03 14:16:30 +0500

    Populated the assignee select component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,17 +1,35 @@
 'use client'
 
+import { User } from "@prisma/client"
 import { Select } from "@radix-ui/themes"
+import axios from "axios"
 
 import React from 'react'
 
 const AssigneeSelect = () => {
+  const [users, setUsers] = React.useState<User[]>([])
+
+  React.useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get('/api/users');
+        setUsers(response.data);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    }    
+
+    fetchUsers()
+  }, [])
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign..."/>
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="1">Masha</Select.Item>
+          {users.map((user) => (
+            <Select.Item value={user.id}>{user.name}</Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
