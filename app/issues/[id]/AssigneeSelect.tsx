aebcc08dae35443e89672590c119e9ede7a0b197
misha133kv <mihail_fomin_95@mail.ru>
2024-02-04 11:52:09 +0500

    Added unassigned option

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -21,7 +21,7 @@ const AssigneeSelect = ({issue}: { issue: Issue }) => {
   const assignIssue = (userId: string) => {
     try {
       axios.patch('/api/issues/' + issue.id, {
-        assignedTo: userId || null
+        assignedTo: userId === 'unassigned' ? null : userId
       })
       toast.success('Issue has been assigned to User')
     } catch (error) {
@@ -40,7 +40,7 @@ const AssigneeSelect = ({issue}: { issue: Issue }) => {
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          {/* <Select.Item value=''>Unassigned</Select.Item> */}
+          <Select.Item value='unassigned'>Unassigned</Select.Item>
           {users?.map((user) => (
             <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>
           ))}
