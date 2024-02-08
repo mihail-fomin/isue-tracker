307a845d55e75eefab930a908cf285e71140595c
misha133kv <mihail_fomin_95@mail.ru>
2024-02-08 21:58:14 +0500

    Refactor: Extract IssueTable

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -8,23 +8,17 @@ import { Issue, Status } from '@prisma/client'
 import NextLink from 'next/link'
 import { ArrowUpIcon } from '@radix-ui/react-icons'
 import Pagintaion from '@/app/components/Pagintaion'
+import IssueTable, { IssueQuery, columnNames } from './IssueTable'
 
 interface Props {
-  searchParams: { status: Status; orderBy: keyof Issue; page: string }
+  searchParams: IssueQuery
 }
 
 const IssuesPage = async ({ searchParams }: Props) => {
-  const columns: { label: string; value: keyof Issue; className?: string }[] = [
-    { label: 'Issue', value: 'title' },
-    { label: 'Status', value: 'status', className: 'hidden md:table-cell' },
-    { label: 'CreatedAt', value: 'createdAt', className: 'hidden md:table-cell' },
-  ]
   const statuses = Object.values(Status)
   const status = statuses.includes(searchParams.status) ? searchParams.status : undefined
 
-  const orderBy = columns.map((column) => column.value).includes(searchParams.orderBy)
-    ? { [searchParams.orderBy]: 'asc' }
-    : undefined
+  const orderBy = columnNames.includes(searchParams.orderBy) ? { [searchParams.orderBy]: 'asc' } : undefined
 
   const page = parseInt(searchParams.page) || 1
   const PAGE_SIZE = 10
@@ -43,40 +37,7 @@ const IssuesPage = async ({ searchParams }: Props) => {
   return (
     <div className="mb-5">
       <IssueActions />
-      <Table.Root variant="surface">
-        <Table.Header>
-          <Table.Row>
-            {columns.map((column) => (
-              <TableColumnHeaderCell key={column.value} className={column.className}>
-                <NextLink
-                  href={{
-                    query: { ...searchParams, orderBy: column.value },
-                  }}
-                >
-                  {column.label}
-                </NextLink>
-                {column.value === searchParams.orderBy && <ArrowUpIcon className="inline" />}
-              </TableColumnHeaderCell>
-            ))}
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {issues.map((issue) => (
-            <Table.Row key={issue.id}>
-              <Table.Cell>
-                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
-                <div className="block md:hidden">
-                  <IssueStatusBadge status={issue.status} />
-                </div>
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
-                <IssueStatusBadge status={issue.status} />
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">{issue.createdAt.toDateString()}</Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
+      <IssueTable searchParams={searchParams} issues={issues} />
       <Pagintaion pageSize={PAGE_SIZE} currentPage={page} itemCount={issueCount} />
     </div>
   )
