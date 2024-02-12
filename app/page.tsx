7bb9cf9b8438c27f2c8421837858d7de5874268e
misha133kv <mihail_fomin_95@mail.ru>
2024-02-12 21:21:31 +0500

    Built the LastestIssue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Pagination from './components/Pagintaion'
+import LatestIssues from './LatestIssues'
 
-export default function Home({ searchParams }: { searchParams: { page: string } }) {
-  return <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)} />
+export default function Home() {
+  return <LatestIssues />
 }
