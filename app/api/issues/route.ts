83daa6414cabc326ed8ac46d809b3dd87b83604c
misha133kv <mihail_fomin_95@mail.ru>
2024-02-17 16:53:31 +0500

    Added Telegram sendMessage when creating new issue

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -3,9 +3,22 @@ import prisma from '@/app/utils/connect'
 import { issueSchema } from '../issueSchema'
 import { getServerSession } from 'next-auth'
 import authOptions from '@/app/auth/authOptions'
+import TelegramBot from 'node-telegram-bot-api'
+
+const telegramToken = process.env.TG_TOKEN
+const bot = new TelegramBot(telegramToken, { polling: true })
+const chatId = '-4157860383'
+
 
 export async function POST(req: NextRequest) {
   const body = await req.json()
+  console.log('body: ', body);
+
+  const {
+    title,
+    description
+  } = body
+
   const validation = issueSchema.safeParse(body)
 
   const session = await getServerSession(authOptions)
@@ -18,6 +31,14 @@ export async function POST(req: NextRequest) {
   const newIssue = await prisma.issue.create({
     data: { title: body.title, description: body.description },
   })
+  
+  const message = `
+    <strong>Новая задача:</strong> ${title}
+    <b>Описание задачи:</b> ${description}
+    `
+
+  bot.sendMessage(chatId, message, { parse_mode: 'HTML' })
+
 
   return NextResponse.json(newIssue, { status: 201 })
 }
