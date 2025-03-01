d360ede888df0958fb997993d2dfe00a5e7369cc
Misha <mihail_fomin_95@mail.ru>
2025-03-01 13:36:05 +0500

    Revert backto the google auth

--- app/auth/authOptions.ts
+++ app/auth/authOptions.ts
@@ -1,10 +1,10 @@
+import GoogleProvider from 'next-auth/providers/google'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import prisma from '@/app/utils/connect'
 import { NextAuthOptions } from 'next-auth'
-import VkProvider from "next-auth/providers/vk";
 
-const clientId = process.env.VK_CLIENT_ID
-const clientSecret = process.env.VK_CLIENT_SECRET
+const clientId = process.env.GOOGLE_CLIENT_ID
+const clientSecret = process.env.GOOGLE_CLIENT_SECRET
 
 if (!clientId || !clientSecret) {
   throw new Error('VK_CLIENT_ID and VK_CLIENT_SECRET must be defined in your environment')
@@ -13,19 +13,11 @@ if (!clientId || !clientSecret) {
 const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
-    VkProvider({
-        clientId,
-        clientSecret,
-    })
+    GoogleProvider({
+      clientId,
+      clientSecret,
+    }),
   ],
-  callbacks: {
-    async signIn({ user, account }) {
-      if (account) {
-        account.user_id = String(account.user_id);
-      }
-      return true;
-    },
-  },
   session: {
     strategy: 'jwt',
     maxAge: 30 * 24 * 60 * 60, // 30 days
